a1031c9b3f27729280eb3fc839dd4fe7263d92c9
David Disseldorp <ddiss@suse.de>
2023-04-04 23:43:24 +0200

    samba-dfs: add cut and autorun
    Setup a Samba share and create a DFS link to
    ${CIFS_DFS_SERVER}\\${CIFS_DFS_SHARE}
    
    TODO:
    - document new rapido.conf params
    
    Signed-off-by: David Disseldorp <ddiss@suse.de>

---
+++ autorun/samba_dfs.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# SPDX-License-Identifier: (LGPL-2.1 OR LGPL-3.0)
+# Copyright (C) SUSE LLC 2017-2023, all rights reserved.
+
+_vm_ar_env_check || exit 1
+
+set -x
+
+_vm_ar_dyn_debug_enable
+
+[ -n "$CIFS_DFS_SERVER" ] || _fatal "CIFS_DFS_SERVER setting required"
+[ -n "$CIFS_DFS_SHARE" ] || _fatal "CIFS_DFS_SHARE setting required"
+
+# use a non-configurable UID/GID for now
+cifs_xid="579120"
+echo "${CIFS_USER}:x:${cifs_xid}:${cifs_xid}:Samba user:/:/sbin/nologin" \
+	>> /etc/passwd
+echo "${CIFS_USER}:x:${cifs_xid}:" >> /etc/group
+
+mkdir -m 777 -p /mnt/
+ln -s "msdfs:${CIFS_DFS_SERVER}\\${CIFS_DFS_SHARE}" /mnt/dfs-redir
+
+cfg_file="/smb.conf"
+cat > "$cfg_file" << EOF
+[global]
+	workgroup = MYGROUP
+	load printers = no
+	smbd: backgroundqueue = no
+
+[${CIFS_SHARE}]
+	msdfs root = yes
+	path = /mnt
+	read only = no
+	store dos attributes = yes
+EOF
+
+set +x
+_samba_paths_init "$cfg_file"
+
+smbd -s "$cfg_file" || _fatal
+
+echo -e "${CIFS_PW}\n${CIFS_PW}\n" \
+	| smbpasswd -c "$cfg_file" -a $CIFS_USER -s || _fatal
+
+pub_ips=()
+_vm_ar_ip_addrs_nomask pub_ips
+for i in "${pub_ips[@]}"; do
+	echo "Samba share ready at: //${i}/${CIFS_SHARE}/" \
+		"with DFS redirect to ${CIFS_DFS_SERVER}\\${CIFS_DFS_SHARE}"
+done
